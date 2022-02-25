0d8b9c2946ac4dec9fcee1a24c4799370ca9f533
Jacob Evans <jacob@dekz.net>
2022-02-25 11:58:20 +1000

    fix: Large RPC ID

--- src/rpc-client.js
+++ src/rpc-client.js
@@ -19,7 +19,7 @@ module.exports = class RpcClient {
 	constructor(provider) {
 		this.provider = provider;
 		this._chainId = null;
-		this._nextRpcId = _.random(1, 2**64);
+		this._nextRpcId = _.random(1, 2**32);
 	}
 
 	async getDefaultAccount() {
